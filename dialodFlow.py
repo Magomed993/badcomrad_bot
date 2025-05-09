399c5d506b93deaa23ab759b78d89e533b288e86
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2025-05-09 20:32:28 +0300

    Add DialogFlow learning script

--- dialodFlow.py
+++ dialodFlow.py
@@ -1,3 +1,4 @@
+import json
 from google.cloud import dialogflow
 
 
@@ -31,5 +32,40 @@ Fulfillment text: {response.query_result.fulfillment_text}\n''')
         return response.query_result.fulfillment_text
 
 
+with open('intents.json', 'r') as my_file:
+    intents_json = my_file.read()
+INTENTS = json.loads(intents_json)
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
 if __name__ == '__main__':
     detect_intent_texts('magic-fjpn', '123456789', ['Хай'], 'ru-RUS')
+    for intent in INTENTS:
+        create_intent('magic-fjpn', intent, INTENTS.get(intent)['questions'], [INTENTS.get(intent)['answer']])
